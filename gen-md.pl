290f4b448c249670b120bb127e4efa73b2a2153f
PeterCJ <pryrtcode@pryrt.com>
2020-02-04 12:24:20 -0800

    okay, added to udl-list.md

--- gen-md.pl
+++ gen-md.pl
@@ -6,24 +6,30 @@ use autodie;
 use JSON::PP;
 use 5.010;
 use Data::Dumper; $Data::Dumper::Useqq=1;
+use Encode qw'encode decode';
 
 $|++;
-binmode STDOUT, ':encoding(UTF-8)';
+#binmode STDOUT, ':encoding(UTF-8)';    # this was double-encoding, apparently.
 
 for my $fname ( @ARGV ) {
-    print "file($fname):\n";
-    my $struct = do {local $/; open my $ih, '<', $fname; JSON::PP::decode_json(<$ih>) };
-    print "file($fname): $struct\n";
+    warn "file($fname)\n";
+    my $struct = do {local $/; open my $ih, '<:raw', $fname; JSON::PP::decode_json(<$ih>) };
     for my $h ( @$struct ) {
         my $f;
         $f = "./UDLs/" . $h->{'id-name'} . ".xml";
-        warn "try '$f'\n";
         $f = $h->{'repository'} unless -f $f;
-        unless(defined $f) {
-            warn "File not defined! ", Dumper $h;
-            next;
+        die "no file found", Dumper($h) unless $f;
+        my $display = $h->{'display-name'};
+        $display = sprintf '[%s](%s)', $display, $f if $f;
+        my $desc = $h->{'description'} || $h->{'display-name'};
+        my $auth = $h->{'author'};
+        $auth =~ m/^\s*<(.*?)>/ && do { $auth = $1 };
+        $auth =~ s/\s*<.*?>\s*//;
+        $auth =~ m/^mailto:(.*)\@.*$/ && do { $auth = $1 };
+        if($h->{homepage}) {
+            $auth = sprintf '[%s](%s)', $auth, $h->{homepage};
         }
-        printf qq(| %s |\n), $f||'<empty>';
-        die "empty" unless $f;
+        my $txt = sprintf qq(| %s | %s | %s |\n), $display, $desc, $auth;
+        print encode('UTF-8', $txt );
     }
 }
