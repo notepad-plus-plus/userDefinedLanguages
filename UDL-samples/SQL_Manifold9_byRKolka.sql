89fac8b4021a67152140178852a969da5533e54a
Riivo Kolka <riivo.kolka@gmail.com>
2022-10-06 12:40:36 +0300

    SQL_Manifold9_byRKolka: add { } to Operators 1, change nesting, add more samples

--- UDL-samples/SQL_Manifold9_byRKolka.sql
+++ UDL-samples/SQL_Manifold9_byRKolka.sql
@@ -7,6 +7,8 @@
 -- Symbol-like "operators", that do not require surrounding whitespace and can itself act as separators between words.
 -- Operators 1                                        «%», «&», «*», «+», «-», «/», «<», «<=», «<>», «=», «>», «>=», «^», «,», «::», «.», «;», «(», «)»
 -- Should «(», «)»  be here, or Delimiters 6?
+-- Should «{», «}»  be here as part of JSON strings
+
 
 -- Word-like "operators", that require surrounding whitespace or separators
 -- Operators 2                                        «AND», «BETWEEN», «BITAND», «LIKE», «MOD», «NOT», «OR», «SPLIT», «UNION», «UNION ALL», «XOR», etc.
@@ -16,6 +18,8 @@
 -- Delimiter pairs
 -- Order of {1, 2} and {3, 4, 5} is important
 -- Delimiter 1  escapeless string quotes                 «@'»  «'»   
+--    NOTE: Operators 1 are nested within quotes to highligth commas inside JSON
+--    NOTE: Should Delimiter 5 be nested within quotes to highligth double-quoted strings and values inside JSON?
 -- Delimiter 2  string quotes    with «\» escape         «'»   «'»  
 -- Delimiter 3  context and expression double-brackets   «[[»  «]]»
 -- Delimiter 4  function parameter brackets              «@[»  «]»
@@ -72,11 +76,12 @@ SELECT * FROM [Files] WHERE [Path] like @'C:\no\escape\needed\' ;
 SELECT * FROM [Orders] WHERE [City] = @'London' ;
 -- }}
 
+
 -- {{  Delimiter 2  «'»   «'»  sample
 SELECT * FROM [Orders] WHERE [City] = 'London' ;
 SELECT * FROM [Orders] WHERE [City] = '\'s-Hertogenbosch' ;
-
 -- }}
+
 
 --  {{  Delimiter 3  «[[»  «]]»  sample
 EXECUTE WITH (@x INT32 = 2, @y INT32 = 20) [[ INSERT INTO dbo.t (a, b) VALUES (@x@, @y@) ]] ON [sql];
@@ -90,6 +95,7 @@ EXECUTE WITH (@n TABLE = [States Table])
 ;
 -- }} 
 
+
 --  {{  Delimiter 4 «@[»  «]» / Keyword 7  «@» sample
 FUNCTION f(@[ a crazy param name $ 5@six.com ] TABLE, @not_bigger_than INT32) TABLE AS
 (
@@ -99,6 +105,7 @@ END
 ;
 -- }} 
 
+
 --  {{  Delimiter 5 «[» «]», «`» «`», «"» «"»  sample
 SELECT * FROM [2016 Roads];
 SELECT * FROM `2016 Roads`;
@@ -107,9 +114,11 @@ SELECT [Population 1990] FROM [Countries];
 SELECT "Population 1990" FROM `Countries`;
 -- }} 
 
+
 --  {{  Delimiter 7 «#» «#» sample
 SELECT * FROM [Orders] WHERE [datetime] > #01/21/2017 12:05:15# ;
 -- }} 
+
 
 --  {{  Delimiter 8 «!», «?» sample
 !fullfetch
@@ -124,7 +133,7 @@ SELECT * FROM [Orders] WHERE [datetime] > #01/21/2017 12:05:15# ;
  
  
 --  {{  Keywords 8 sample
--- Keywords 8 are inside quotes
+-- Delimiter 2 single-quotes nest Keywords 8 (and Operators 1 and Delimiter 5)
 CREATE TABLE [Table] (
 	[name] NVARCHAR,
 	INDEX [name_x] BTREENULL ([name] COLLATE 'en-US, nocase, noaccent')
@@ -136,7 +145,8 @@ PRAGMA ('gpgpu'='aggressive', 'gpgpu.fp'='32')
 
 
 
---  {{  Other samples
+--  {{  Tables from datasources using «::» sample
+
 SELECT 
 	[USA States].[State], 
 	[USA States].[Population], 
@@ -148,11 +158,50 @@ FROM
 	ON 
 	[USA States].[State] = [City Capitals].[State]
 ;
+--}}
 
-  
-FUNCTION combine(@p NVARCHAR, @q NVARCHAR) NVARCHAR AS
- @p & ': ' & @q  END;
+--  {{  Function sample
+-- Pure SQL/9 function
+FUNCTION combine(@p NVARCHAR, @q NVARCHAR) NVARCHAR AS @p & ': ' & @q  END;
 
-FUNCTION F(@x FLOAT64) FLOAT64
-AS SCRIPT FILE 'math2.dll' ENTRY 'math.Var.F';
+-- Function from dll
+FUNCTION F(@x FLOAT64) FLOAT64 AS SCRIPT FILE 'math2.dll' ENTRY 'math.Var.F';
+--}}
+
+--  {{  JSON double-quoted values in single-quoted string sample
+-- Note: Delimiter 2 single-quotes nest Delimiter 5 and Operators 1 (and Keywords 8)
+CREATE TABLE [ContinuousColor Table] (
+  [mfd_id] INT64,
+  [Geom] GEOM,
+  [v] FLOAT32,
+  [x] FLOAT32,
+  [y] FLOAT32,
+  INDEX [mfd_id_x] BTREE ([mfd_id]),
+  INDEX [Geom_x] RTREE ([Geom]),
+  PROPERTY 'FieldCoordSystem.Geom' 'EPSG:3301,mfd:{ "Axes": "XY" }'
+);
+
+CREATE DRAWING [ContinuousColor] (
+  PROPERTY 'FieldGeom' 'Geom',
+  PROPERTY 'StylePointColor' '{ "Value": 11119017 }',
+  PROPERTY 'StylePointColorBack' '{ "Field": "v", "Fill": "boundaverage", "Value": 16777215, "Values": { "0": 16777215, "10": 3688006, "15": 15594514, "20": 16777215, "5": 2287079 } }',
+  PROPERTY 'StylePointSize' '{ "Value": 8 }',
+  PROPERTY 'Table' '[ContinuousColor Table]'
+);
+
+-- Query in single-quoted string
+CREATE QUERY [Insert_ContinuousColor] (
+  PROPERTY 'Text' '
+  -- $manifold$
+  DELETE FROM [ContinuousColor Table];
+  INSERT INTO [ContinuousColor Table] ( [v], [x], [y] )
+  SELECT 
+	[Value]*20 as [v],
+	Trunc([Value]*40*40) div 40 as [x],
+	Trunc([Value]*40*40) mod 40 as [y]
+  FROM
+	CALL ValueSequenceRandom(500, 111)
+  ;
+'
+);
 --}}
