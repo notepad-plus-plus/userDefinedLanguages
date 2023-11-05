de20469e4c167858a3d0db6761737cdd97313a74
PeterCJ <pryrtcode@pryrt.com>
2023-11-05 13:35:22 -0800

    re-merge the validator and markdown generator into one action

--- .validators/validator_json.py
+++ .validators/validator_json.py
@@ -60,23 +60,53 @@ def rest_of_text(description):
     return description[len(first_two_lines(description)):]
 
 def gen_pl_table(filename):
-    udlfile = json.loads(open(filename, encoding="utf8").read())
-    tab_text = "## UDL Definitions%s" % (tmpl_new_line)
+    try:
+        udlfile = json.loads(open(filename, encoding="utf8").read())
+    except ValueError as e:
+        post_error(filename + " - " + str(e))
+        return
+
+    tab_text = "## UDL Definitions%s%s" % (tmpl_new_line, tmpl_new_line)
     # tab_text += "version %s%s" % (udlfile["version"], tmpl_new_line)
     tab_text += tmpl_tab_head
+
+    ac_list = []
 
     # UDL Name = (ij.display-name)ij.id-name.xml or repolink
     # Author = ij.author
     # Description = " <details> <summary> " + first_two_lines(ij.description) + " </summary> " rest_of_text(ij.description) +"</details>"
-    for udl in sorted(udlfile["UDLs"], key=lambda d: d['display-name']):
+    for udl in sorted(udlfile["UDLs"], key=lambda d: d['display-name'].casefold()):
+        # link to either repo or local copy of XML
         udl_link = udl["repository"]
         if not udl_link:
             udl_link = "./UDLs/" + udl["id-name"] + ".xml"
-        tab_line = tmpl_tr_b + "[" + udl["display-name"] +"](" + udl_link + ")" + tmpl_td + udl["author"] + tmpl_td
+
+        # author name (with optional link to homepage)
+        mailto = ""
+        if ' <mailto:' in udl["author"]:
+            p = udl["author"].find(' <mailto:')
+            m = p + 2
+            e = udl["author"].find('>', p)
+            mailto = udl["author"][m:e]
+            author = udl["author"][:p]
+        else:
+            author = udl["author"]
+
+        if 'homepage' in udl:
+            author = "[%s](%s)" % (author, udl["homepage"])
+        elif mailto:
+            author = "[%s](%s)" % (author, mailto)
+
+        # concat name and author
+        tab_line = tmpl_tr_b + "[" + udl["display-name"] +"](" + udl_link + ")" + tmpl_td + author + tmpl_td
+
+        # grab description, and summarize if it's long...
         descr = udl["description"]
         descr = descr.replace(c_line_feed, tmpl_br).replace(c_line_break, '').replace("|", tmpl_vert)
         summary = first_two_lines(descr)
         rest = rest_of_text(descr)
+
+        # add description to the current table row
         if summary:
             tab_line += " <details> <summary> %s </summary> %s </details>" % (summary, rest)
         else:
@@ -84,7 +114,50 @@ def gen_pl_table(filename):
         tab_line += tmpl_tr_e + tmpl_new_line
         tab_text += tab_line
 
+        # if this entry has autoCompletion defined, add it to the list of autoCompletions
+        if "autoCompletion" in udl:
+            if udl["autoCompletion"]:
+                if str(udl["autoCompletion"]) == "True":
+                    ac_link = udl["id-name"] + ".xml"
+                elif udl["autoCompletion"][0:4] == "http":
+                    ac_link = udl["autoCompletion"]
+                else:
+                    ac_link = str(udl["autoCompletion"]) + ".xml"
+
+                # print(f'autoCompletion: {udl["autoCompletion"]} => {ac_link}')
+                # absolute path for existence testing
+                ac_link_abs  = Path(os.path.join(os.getcwd(),"autoCompletions", ac_link))
+
+                # relative path for correct linking
+                ac_link = "./autoCompletions/%s" % (ac_link)
+
+                # TODO: use autoCompletionAuthor field if the autoCompletion has a different author than the UDL (like for RenderMan)
+                if "autoCompletionAuthor" in udl:
+                    if udl["autoCompletionAuthor"]:
+                        author = udl["autoCompletionAuthor"]
+                        mailto = ""
+                        if ' <mailto:' in udl["autoCompletionAuthor"]:
+                            p = udl["autoCompletionAuthor"].find(' <mailto:')
+                            m = p + 2
+                            e = udl["autoCompletionAuthor"].find('>', p)
+                            mailto = udl["autoCompletionAuthor"][m:e]
+                            author = udl["autoCompletionAuthor"][:p]
+
+                # append to list if it exists, otherwise give error
+                if not ("http:" in ac_link or "https:" in ac_link) and not ac_link_abs.exists():
+                    print(f'ac_link = {ac_link}')
+                    post_error(f'{udl["display-name"]}: autoCompletion file missing from repo: JSON id-name expects it at filename="{ac_link}"')
+                else:
+                    ac_list.append(tmpl_tr_b + "[" + udl["display-name"] +"](" + ac_link + ")" + tmpl_td + author + tmpl_td + udl["description"] + tmpl_tr_e)
+
+    # add the Auto-Completion Definitions in a separate table at the end
+    tab_text += tmpl_new_line
+    tab_text += "## Auto-Completion Definitions%s%s" % (tmpl_new_line, tmpl_new_line)
+    tab_text += tmpl_tab_head
+    tab_text += tmpl_new_line.join(ac_list)
+
     return tab_text
+
 
 def parse(filename):
     try:
@@ -186,8 +259,8 @@ def parse(filename):
 
 
 parse("udl-list.json")
-#with open("udl-list.md", "w", encoding="utf8") as md_file:
-#    md_file.write(gen_pl_table("udl-list.json"))
+with open("udl-list.md", "w", encoding="utf8") as md_file:
+    md_file.write(gen_pl_table("udl-list.json"))
 
 if has_error:
     sys.exit(-2)
