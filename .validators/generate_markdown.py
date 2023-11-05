41070f836ae327e8c50d3df14a69a90db447485e
PeterCJ <pryrtcode@pryrt.com>
2023-11-05 11:07:45 -0800

    clean up the generator
    use the generated output for udl-list.md for now

--- .validators/generate_markdown.py
+++ .validators/generate_markdown.py
@@ -6,8 +6,6 @@ import io
 import sys
 
 import requests
-from hashlib import sha256
-from jsonschema import Draft202012Validator, FormatChecker
 from pathlib import Path
 
 api_url = os.environ.get('APPVEYOR_API_URL')
@@ -76,14 +74,37 @@ def gen_pl_table(filename):
     # Author = ij.author
     # Description = " <details> <summary> " + first_two_lines(ij.description) + " </summary> " rest_of_text(ij.description) +"</details>"
     for udl in sorted(udlfile["UDLs"], key=lambda d: d['display-name'].casefold()):
+        # link to either repo or local copy of XML
         udl_link = udl["repository"]
         if not udl_link:
             udl_link = "./UDLs/" + udl["id-name"] + ".xml"
-        tab_line = tmpl_tr_b + "[" + udl["display-name"] +"](" + udl_link + ")" + tmpl_td + udl["author"] + tmpl_td
+
+        # author name (with optional link to homepage)
+        mailto = ""
+        if ' <mailto:' in udl["author"]:
+            p = udl["author"].find(' <mailto:')
+            m = p + 2
+            e = udl["author"].find('>', p)
+            mailto = udl["author"][m:e]
+            author = udl["author"][:p]
+        else:
+            author = udl["author"]
+
+        if 'homepage' in udl:
+            author = "[%s](%s)" % (author, udl["homepage"])
+        elif mailto:
+            author = "[%s](%s)" % (author, mailto)
+
+        # concat name and author
+        tab_line = tmpl_tr_b + "[" + udl["display-name"] +"](" + udl_link + ")" + tmpl_td + author + tmpl_td
+
+        # grab description, and summarize if it's long...
         descr = udl["description"]
         descr = descr.replace(c_line_feed, tmpl_br).replace(c_line_break, '').replace("|", tmpl_vert)
         summary = first_two_lines(descr)
         rest = rest_of_text(descr)
+
+        # add description to the current table row
         if summary:
             tab_line += " <details> <summary> %s </summary> %s </details>" % (summary, rest)
         else:
@@ -91,118 +112,37 @@ def gen_pl_table(filename):
         tab_line += tmpl_tr_e + tmpl_new_line
         tab_text += tab_line
 
-        # if "autoCompletion" in udl:
-        #     if udl["autoCompletion"]:
-        #         if str(udl["autoCompletion"]) == "True":
-        #             ac_link = udl["id-name"] + ".xml"
-        #         else:
-        #             ac_link = str(udl["autoCompletion"]) + ".xml"
-        #         ac_link_abs  = Path(os.path.join(os.getcwd(),"autoCompletions", ac_link))
-        #         if not ac_link_abs.exists():
-        #             post_error(f'{udl["display-name"]}: autoCompletion file missing from repo: JSON id-name expects it at filename="autoCompletions/{ac_link}"')
-        #         else:
-        #             ac_list.append(tmpl_tr_b + "[" + udl["display-name"] +"](" + udl_link + ")" + tmpl_td + udl["author"] + tmpl_td + udl["description"] + tmpl_tr_e)
-
-    # print("what's wrong?")
-
-    # add the Auto-Completion Definitions in a separate table at the end
-    #tab_text += tmpl_new_line
-    #tab_text += "## Auto-Completion Definitions%s%s" % (tmpl_new_line, tmpl_new_line)
-    #tab_text += tmpl_tab_head
-    #tab_text += tmpl_new_line.join(ac_list)
-
-    return tab_text
-
-def parse(filename):
-    try:
-        schema = json.loads(open(".validators/udl.schema").read())
-        schema = Draft202012Validator(schema, format_checker=FormatChecker())
-    except ValueError as e:
-        post_error("udl.schema - " + str(e))
-        return
-
-    try:
-        udlfile = json.loads(open(filename, encoding="utf8").read())
-    except ValueError as e:
-        post_error(filename + " - " + str(e))
-        return
-
-    for error in schema.iter_errors(udlfile):
-        post_error(error.message)
-
-    idnames = []
-    displaynames = []
-    repositories = []
-    response = []
-
-    for udl in udlfile["UDLs"]:
-        print(udl["display-name"])
-
-        try:
-            if udl["repository"] != "" :
-                response = requests.get(udl["repository"])
-        except requests.exceptions.RequestException as e:
-            post_error(str(e))
-            continue
-
-        if udl["repository"] != "" and response.status_code != 200:
-            post_error(f'{udl["display-name"]}: failed to download udl from repository="{udl["repository"]}". Returned code {response.status_code}')
-            continue
-
-        # check if file exists in this repo if no external link is available
-        if not udl["repository"]:
-            udl_link = udl["id-name"] + ".xml"
-            udl_link_abs  = Path(os.path.join(os.getcwd(),"UDLs", udl_link))
-            if not udl_link_abs.exists():
-                post_error(f'{udl["display-name"]}: udl file missing from repo: JSON id-name expects it at filename="{udl_link}"')
-
-        # Hash it and make sure its what is expected
-        #hash = sha256(response.content).hexdigest()
-        #if udl["id"].lower() != hash.lower():
-        #    post_error(f'{udl["display-name"]}: Invalid hash. Got {hash.lower()} but expected {udl["id"]}')
-        #    continue
-
-        #check uniqueness of json id-name, display-name and repository
-        found = False
-        for name in displaynames :
-           if udl["display-name"] == name :
-               post_error(f'{udl["display-name"]}: non unique display-name entry')
-               found = True
-        if found == False:
-               displaynames.append(udl["display-name"])
-
-        found = False
-        for idname in idnames :
-           if udl["id-name"] == idname :
-               post_error(f'{udl["id-name"]}: non unique id-name entry')
-               found = True
-        if found == False:
-           idnames.append(udl["id-name"])
-
-        found = False
-        for repo in repositories :
-           if udl["repository"] != "" and udl["repository"] == repo :
-                   post_error(f'{udl["repository"]}: non unique repository entry')
-                   found = True
-        if found == False:
-           repositories.append(udl["repository"])
-
-        # look at optional autoCompletion
+        # if this entry has autoCompletion defined, add it to the list of autoCompletions
         if "autoCompletion" in udl:
-            # print(f'\tautoCompletion: {udl["autoCompletion"]}')
             if udl["autoCompletion"]:
                 if str(udl["autoCompletion"]) == "True":
                     ac_link = udl["id-name"] + ".xml"
                 else:
                     ac_link = str(udl["autoCompletion"]) + ".xml"
+
+                # absolute path for existence testing
                 ac_link_abs  = Path(os.path.join(os.getcwd(),"autoCompletions", ac_link))
+
+                # relative path for correct linking
+                ac_link = "./autoCompletions/%s" % (ac_link)
+
+                # TODO: use autoCompleteAuthor field if the autoCompletion author is different than the UDL author (like for RenderMan)
+
+                # append to list if it exists, otherwise give error
                 if not ac_link_abs.exists():
-                    post_error(f'{udl["display-name"]}: autoCompletion file missing from repo: JSON id-name expects it at filename="autoCompletions/{ac_link}"')
+                    post_error(f'{udl["display-name"]}: autoCompletion file missing from repo: JSON id-name expects it at filename="{ac_link}"')
                 else:
-                    print(f'-> also confirmed "autoCompletions/{ac_link}"')
+                    ac_list.append(tmpl_tr_b + "[" + udl["display-name"] +"](" + ac_link + ")" + tmpl_td + author + tmpl_td + udl["description"] + tmpl_tr_e)
 
+    # add the Auto-Completion Definitions in a separate table at the end
+    tab_text += tmpl_new_line
+    tab_text += "## Auto-Completion Definitions%s%s" % (tmpl_new_line, tmpl_new_line)
+    tab_text += tmpl_tab_head
+    tab_text += tmpl_new_line.join(ac_list)
 
-with open("udl-list2.md", "w", encoding="utf8") as md_file:
+    return tab_text
+
+with open("udl-list.md", "w", encoding="utf8") as md_file:
     md_file.write(gen_pl_table("udl-list.json"))
 
 if has_error:
