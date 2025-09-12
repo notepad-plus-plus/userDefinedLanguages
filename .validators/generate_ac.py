cfa087e8e2217fa4427a8acfde7c7c5fab6e03d7
PeterCJ <pryrtcode@pryrt.com>
2025-09-12 15:14:13 -0700

    Update generate_ac: don't include prefix-style keywords
    delete all the old auto-generated from folder and JSON, so they will be regenerated

--- .validators/generate_ac.py
+++ .validators/generate_ac.py
@@ -57,11 +57,13 @@ def getUdlInfoAndKeywords(udl):
     if 'repository' in udl and udl['repository'][0:4] == 'http':
         return None
 
+    # make sure UDL XML file exists
     udl_filename = f"./UDLs/{udl['id-name']}.xml"
     if not os.path.exists(udl_filename):
         post_error(f'getUdlInfoAndKeywords("{udl_filename}"): File does not exist')
         return None
 
+    # parse the UDL XML
     try:
         udl_doc = etree.parse(udl_filename)
     except IOError:
@@ -74,35 +76,62 @@ def getUdlInfoAndKeywords(udl):
         post_error(f'getUdlInfoAndKeywords("{udl_filename}"): Unknown error. Maybe check that no xml version is in the first line.')
         return None
 
+    # Get the UserLang element
     el_userlang = udl_doc.find(".//UserLang")
     if el_userlang is None:
         post_error(f'getUdlInfoAndKeywords("{udl_filename}"): no <UserLang> found')
         return None
 
+    # Extract the name attribute from <UserLang>
     if not 'name' in el_userlang.attrib:
         post_error(f'getUdlInfoAndKeywords("{udl_filename}"): no <UserLang> found')
         return None
 
     xinfo["udl_internal_name"] = el_userlang.get('name')
 
+    # Get the Prefix information
+    el_prefix = el_userlang.find('.//Prefix')
+    prefix = dict(Keywords1="no",Keywords2="no",Keywords3="no",Keywords4="no",Keywords5="no",Keywords6="no",Keywords7="no",Keywords8="no")
+    if el_prefix is None:
+        post_error(f'getUdlInfoAndKeywords("{udl_filename}"): could not find <Prefix> element')
+        return None
+
+    #print(f'{xinfo["udl_internal_name"]}:: found {etree.tostring(el_prefix).decode("utf-8")}')
+    for key in prefix.keys():
+        if key in el_prefix.attrib:
+            prefix[key] = el_prefix.get(key)
+    #print(f'{xinfo["udl_internal_name"]}:: prefixes: {prefix}')
+
+    # Find the <KeywordLists> element
     el_kwlists = el_userlang.find('.//KeywordLists')
     if el_kwlists is None:
         post_error(f'getUdlInfoAndKeywords("{udl_filename}"): no <KeywordLists> found')
         return None
 
+    # Iterate through the <Keywords> elements, looking for name="Keywords#", and adding those to the extracted list of keywords
     kw_list = []
     for kw in el_kwlists.iter('Keywords'):
+        # element must have name attribute, otherwise cannot determine whether it's a keyword list
         if not 'name' in kw.attrib:
             continue
 
         kw_name = kw.get('name')
-        if kw_name[0:8] == 'Keywords':
-            #print(f'{xinfo["udl_internal_name"]}:: <Keywords name="{kw.get('name')}" .../>')
-            if not kw.text is None:
-                kw_list += kw.text.split()
 
-    #if len(kw_list):
-    #    print(f'{xinfo["udl_internal_name"]}:: All KW = {', '.join(kw_list)}')
+        # if it's not Keywords*, and if the * isn't a digit, then don't try to add these keywords
+        if kw_name[0:8] != 'Keywords' or not kw_name[8].isdigit():
+            continue
+
+        # don't try to add empty keyword lists
+        if kw.text is None:
+            continue
+
+        # don't try tot add to keyword list if we cannot determine whether it's a prefix or not
+        if not kw_name in prefix:
+            continue
+
+        # add to keyword list if this element isn't in prefix mode
+        if prefix[kw_name].lower() == "no":
+            kw_list += kw.text.split()
 
     if len(kw_list) == 0:
         kw_list = None  # need to indicate that it found no keywords, at which point, it shouldn't try to generate the AC file...
